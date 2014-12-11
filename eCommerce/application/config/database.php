234fcead13a1421c76cbe90fdd6695d0ef4d9c2b
Bryant <bryly27@sbcglobal.net>
2014-12-10 22:29:31 -0800

    fixed small errors

--- eCommerce/application/config/database.php
+++ eCommerce/application/config/database.php
@@ -49,10 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['username'] = 'root';
-
 $db['default']['password'] = 'root';
-
-
 $db['default']['database'] = 'eCommerceSports';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
